99e62ef755fea021b9a95574de13ef1af4ce87a7
shahd2344 <139980288+shahd2344@users.noreply.github.com>
2023-08-01 14:40:35 +0300

    task 2 first commite

--- task2.py
+++ task2.py
@@ -1,9 +1,10 @@
+print("Welcome Dear!!!!!!!!!!!!!!! SO Please")
 name=(input("Enter your full name"))
 number= (input("Enter your mobile number for example 05*-****-***\n "))
 year= int(input("Enter year of birth"))
 currentAge=2023
 age=currentAge-year
 number=number.split('-')
-print("Name:         ","Age:","    Mobile Number As A List:")
-print(name+"      ",age,"     ",number)
+print("Name:         ","Age:","    Mobile Number :")
+print(name+"        ",age,"       ",number)
 print("*******************************")
