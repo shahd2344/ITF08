c77dc6bdd671b9fdc1076afe2271f705369c8236
shahd2344 <139980288+shahd2344@users.noreply.github.com>
2023-08-02 16:11:25 +0300

    task 2 first commite

---
+++ function.py
@@ -0,0 +1,17 @@
+n1=int(input("Enter first num"))
+n2=int(input("Enter second num"))
+def sum(n1,n2):
+    sum=n1+n2
+    return sum
+def subtract(n1,n2):
+    sub=n1-n2
+    return sub
+def multiplication(n1,n2):
+    mul=n1*n2
+    return mul
+
+def division(n1,n2):
+    div=n1/n2
+    return div
+
+
